9530e973f121f47ceb88b62f0e515e348b5b3eb3
Motz <mjlane88@gmail.com>
2025-02-19 18:59:27 -0600

    Updated JavaScript for form validation

--- scripts/validation.js
+++ scripts/validation.js
@@ -1,23 +1,13 @@
 const showInputError = (formElement, inputElement, errorMessage) => {
-  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
-  inputElement.classList.add("form__input_type_error");
-  errorElement.textContent = errorMessage;
-  errorElement.classList.add("form__input-error_active");
-  console.log(formElement);
-};
-
-const hideInputError = (formElement, inputElement) => {
-  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
-  inputElement.classList.remove("form__input_type_error");
-  errorElement.classList.remove("form__input-error_active");
-  errorElement.textContent = "";
+  const errorMessageElement = document.querySelector(
+    `#${inputElement.id}-error`
+  );
+  errorMessageElement.textContent = errorMessage;
 };
 
 const checkInputValidity = (formElement, inputElement) => {
   if (!inputElement.validity.valid) {
     showInputError(formElement, inputElement, inputElement.validationMessage);
-  } else {
-    hideInputError(formElement, inputElement);
   }
 };
 
@@ -29,7 +19,7 @@ const setEventListeners = (formElement) => {
   //toggleButtonState(inputList, buttonElement);
 
   inputList.forEach((inputElement) => {
-    inputElement.addEventListener("change", function () {
+    inputElement.addEventListener("input", function () {
       checkInputValidity(formElement, inputElement);
       // toggleButtonState(inputList, buttonElement);
     });
